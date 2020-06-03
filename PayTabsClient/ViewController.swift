dde2b22e9fbc1f93f5629f415694776723306cf1
Mohamed Adly <muhamedadly@gmail.com>
2020-06-03 07:04:59 +0200

    Initial Commit

---
+++ PayTabsClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayTabsClient
+//
+//  Created by Mohamed Adly on 6/3/20.
+//  Copyright © 2020 Mohamed Adly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
